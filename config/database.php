4d67162824d024fcc57b5223c1add6f19b1a902b
rjp2525 <renophilibert@gmail.com>
2024-12-07 09:17:11 -0700

    Updated

--- config/database.php
+++ config/database.php
@@ -112,6 +112,23 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
+        'clickhouse' => [
+            'driver' => 'clickhouse',
+            'host' => env('CLICKHOUSE_HOST'),
+            'port' => env('CLICKHOUSE_PORT', '8123'),
+            'database' => env('CLICKHOUSE_DATABASE', 'default'),
+            'username' => env('CLICKHOUSE_USERNAME', 'default'),
+            'password' => env('CLICKHOUSE_PASSWORD', ''),
+            'timeout_connect' => env('CLICKHOUSE_TIMEOUT_CONNECT', 2),
+            'timeout_query' => env('CLICKHOUSE_TIMEOUT_QUERY', 2),
+            'https' => (bool)env('CLICKHOUSE_HTTPS', null),
+            'retries' => env('CLICKHOUSE_RETRIES', 0),
+            'settings' => [ // optional
+                'max_partitions_per_insert_block' => 300,
+            ],
+            'fix_default_query_builder' => true,
+        ],
+
     ],
 
     /*
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test-clickhouse', function () {
+    try {
+        $version = DB::connection('clickhouse')->select('SELECT version()')->fetchAll();
+        return response()->json(['success' => true, 'version' => $version]);
+    } catch (\Exception $e) {
+        return response()->json(['success' => false, 'error' => $e->getMessage()]);
+    }
 });
